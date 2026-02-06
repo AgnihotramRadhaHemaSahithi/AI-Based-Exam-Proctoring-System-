608655798eb88c78da766da0e4010362358bbebe
Agnihotram Radha Hema Sahithi <agnihotramradha@gmail.com>
2026-02-06 19:20:22 +0530

    Database schema design and table creation

--- database_design.py
+++ database_design.py
@@ -34,7 +34,7 @@ def create_roles_table_if_not_exists():
     connection.commit()
     cursor.close()
     connection.close()
-def create_users_table():
+def create_users_table_if_not_exists():
     connection = get_db_connection()
     cursor = connection.cursor()
     cursor.execute(""" 
@@ -50,8 +50,61 @@ def create_users_table():
     connection.commit()
     cursor.close()
     connection.close()
+def create_exams_table_if_not_exists():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute(""" 
+    CREATE TABLE IF NOT EXISTS exams(
+    exam_id SERIAL PRIMARY KEY,
+    exam_title TEXT NOT NULL,
+    academic_year INT NOT NULL,
+    exam_date TIMESTAMP NOT NULL,
+    exam_duration INT NOT NULL,
+    exam_conducted_by INT REFERENCES users(user_id),
+    exam_status TEXT NOT NULL);
+    """)
+    connection.commit()
+    cursor.close()
+    connection.close()
+def create_questions_table_if_not_exists():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute(""" 
+    CREATE TABLE IF NOT EXISTS questions(
+    question_id SERIAL PRIMARY KEY,
+    exam_id INT REFERENCES exams(exam_id),
+    question TEXT NOT NULL,
+    option_a TEXT NOT NULL,
+    option_b TEXT NOT NULL,
+    option_c TEXT NOT NULL,
+    option_d TEXT NOT NULL,
+    correct_option TEXT NOT NULL,
+    marks INT NOT NULL );
+    """)
+    connection.commit()
+    cursor.close()
+    connection.close()
+def create_monitoring_logs_table_if_not_exists():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute(""" 
+    CREATE TABLE IF NOT EXISTS monitoring_logs(
+    log_id SERIAL PRIMARY KEY,
+    exam_id INT REFERENCES exams(exam_id),
+    student_id INT REFERENCES users(user_id),
+    event_type TEXT NOT NULL,
+    severity_level TEXT NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    action_taken TEXT NULL);
+    """)
+    connection.commit()
+    cursor.close()
+    connection.close()
 create_roles_table_if_not_exists()
-create_users_table()
+create_users_table_if_not_exists()
+create_exams_table_if_not_exists()
+create_questions_table_if_not_exists()
+create_monitoring_logs_table_if_not_exists()
 
 if __name__ == '__main__':
     app.run(debug=True)
